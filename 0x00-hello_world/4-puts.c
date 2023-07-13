e3d4fd696b66ad22f785aaa1c711a7184f29ccc7
root <lkulecho2001@gmail.com>
2023-07-13 05:50:55 -0700

    First C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 int main(void){
- char output = "Programming is like building a multilingual puzzle";
- puts(output);
+ puts("Programming is like building a multilingual puzzle");
 
  return 0;
 	 
