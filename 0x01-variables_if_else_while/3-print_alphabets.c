6c4dd9417d2015031ba19359de697d394a2fafa8
root <lkulecho2001@gmail.com>
2023-07-14 13:13:05 -0700

    Print small and capital letters using putchar

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return : 0
+ */
+int main(void)
+{
+	char ltr;
+
+	ltr = 'a';
+	do {
+		putchar(ltr);
+		ltr++;
+	} while (ltr <= 'z')
+	ltr = 'A';
+	do {
+		putchar(ltr);
+		ltr++;
+	} while (ltr <= 'Z')
+	putchar('\n');
+	return (0);
+}
