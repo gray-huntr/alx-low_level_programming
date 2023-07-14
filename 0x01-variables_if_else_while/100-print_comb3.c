8bb5a21b2ba8307bcbbe8784e0f8b876c406a279
root <lkulecho2001@gmail.com>
2023-07-14 16:31:19 -0700

    Print combination of numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+	int j = i + 1;
+
+	while (i <= 8)
+	{
+		while (j <= 9)
+		{
+			putchar(i);
+			putchar(j);
+			if (i != 8 || j != 9)
+			{
+				putchar('.');
+				putchar(' ');
+			}
+			j++;
+		}
+		i++;
+		j = i + 1;
+	}
+	putchar('\n');
+	return (0);
+}
