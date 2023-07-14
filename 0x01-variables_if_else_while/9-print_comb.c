e1317826d3101b8f0407b4d7cfbe61afdfab488e
root <lkulecho2001@gmail.com>
2023-07-14 15:08:10 -0700

    Print numbers with commas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0-
+ */
+int main(void)
+{
+	int num;
+
+	num = 0;
+	while (num < 10)
+	{
+		putchar('0' + num);
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+}	return (0);
