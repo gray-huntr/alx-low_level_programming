bd6fcefab97e3c6848337d4c83bb8e27a3a5f134
root <lkulecho2001@gmail.com>
2023-07-14 13:56:35 -0700

    Print alphabet excluding some letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ltr;
+
+	ltr = 'a';
+	do {
+		if (ltr != 'q' && ltr != 'e')
+		{
+			putchar(ltr);
+		}
+	} while (ltr <= 'z');
+	putchar('\n');
+	return (0);
+}
