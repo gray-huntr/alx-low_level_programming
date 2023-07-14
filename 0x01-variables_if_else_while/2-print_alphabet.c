3d3327ca6896f365c6ce66d16454236273f442e6
root <lkulecho2001@gmail.com>
2023-07-14 12:48:27 -0700

    Print letters of alphabet using putchars

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	char ltr;
+
+	ltr = 'a';
+	d{
+		putchar(ltr);
+		putchar('\n');
+	} while (ltr <= 'z');
+	return (0);
+}
