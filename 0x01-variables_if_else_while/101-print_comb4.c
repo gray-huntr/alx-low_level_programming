aef088a7cceaf6ec5ad401a69672d75469fa4f5e
root <lkulecho2001@gmail.com>
2023-07-14 17:10:28 -0700

    Print nested combination of numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+	int j = i + 1;
+	int k = j + 1;
+
+	while (i <= 7)
+	{
+		while (j <= 8)
+		{
+			while (k <= 9)
+			{
+				putchars('0' + i);
+				putchars('0' + j);
+				putchars('0' + k);
+				if (i != 7 || j != 8 || k != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				k++;
+			}
+			j++;
+			k = j + 1;
+		}
+		i++;
+		j = i + 1;
+		k = j + 1;
+	}
+	putchar('\n');
+	return (0);
+}
