e7ab7766977194633bf4efe467237467675fd4b5
root <lkulecho2001@gmail.com>
2023-07-18 12:24:35 -0700

    Print alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints alphabet 10x
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int x;
+	char ltr;
+
+	x = 0;
+	ltr = 'a';
+	while (x <= 10)
+	{
+		while (ltr <= 'z')
+		{
+			_putchar(ltr);
+			ltr++;
+		}
+		_putchar('\n');
+	}
+}
