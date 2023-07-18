d1083d6f756948bc3f25005e28a8d540dfc96c86
root <lkulecho2001@gmail.com>
2023-07-18 11:43:45 -0700

    Print a-z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print alphabet - prints a - z
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char ltr;
+
+	while (ltr <= 'z')
+	{
+		_putchar(ltr);
+		ltr++;
+	}
+	_putchar('\n');
+	return (0);
+}
