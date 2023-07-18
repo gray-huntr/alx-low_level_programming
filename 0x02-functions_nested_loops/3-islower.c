85597e366e11fa98d899d9fcf0752d3ff3bae205
root <lkulecho2001@gmail.com>
2023-07-18 12:55:01 -0700

    if else

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower - checks if c is lower
+ * @c: param to be checked
+ * Return: 1 if lowercase
+ */
+int _islower(int c)
+{
+	if (c > 98 && c < 123)
+		_putchar(return '0' + 1);
+	else
+		_putchar(return '0' + 0);
+}
