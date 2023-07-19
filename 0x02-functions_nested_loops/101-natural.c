dcf3574b90df493137144d5b960e977f8560f547
root <lkulecho2001@gmail.com>
2023-07-19 10:32:19 -0700

    Print sum no. divisible by 3 and 5' vi 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int x = 0;
+
+	while (x < 1024)
+	{
+		if ((x % 3 == 0) || (x % 5 == 0))
+		{
+			x = x + x;
+			x++
+		}
+	}
+	printf("%d\n", x);
+	return (0);
+
+}
