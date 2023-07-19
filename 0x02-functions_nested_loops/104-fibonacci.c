20283bb56555af851085d3ef4ea4d17fd37d31f1
root <lkulecho2001@gmail.com>
2023-07-19 15:33:55 -0700

    Fibonacci sequence

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,26 +1,49 @@
 #include <stdio.h>
+
+
 /**
- * main - entry point
- *
- * Return: 0
- */
+* main - fibonacci <3
+*
+*
+* Return:  (Success)
+*/
+
+
 int main(void)
 {
-	unsigned int a = 1;
-	unsigned int b = 2;
-	unsigned int i = 3;
-	unsigned int numfibo = 98;
-	unsigned int result;
+unsigned long int i;
+unsigned long int a = 1;
+unsigned long int z = 2;
+unsigned long int l = 1000000000;
+unsigned long int a1;
+unsigned long int a2;
+unsigned long int z1;
+unsigned long int z2;
 
-	printf("1, 2");
-	while (i <= numfibo)
-	{
-		result = a + b;
-		printf(", %u", result);
-		a = b;
-		b = result;
-		i++;
-	}
-	printf("\n");
-	return (0);
+
+printf("%lu", a);
+
+for (i = 1; i < 91; i++)
+{
+printf(", %lu", z);
+z += a;
+a = z - a;
+}
+
+a1 = (a / l);
+a2 = (a % l);
+z1 = (z / l);
+z2 = (z % l);
+
+for (i = 92; i < 99; ++i)
+{
+printf(", %lu", z1 + (z2 / l));
+printf("%lu", z2 % l);
+z1 = z1 + a1;
+a1 = z1 - a1;
+z2 = z2 + a2;
+a2 = z2 - a2;
+}
+printf("\n");
+return (0);
 }
