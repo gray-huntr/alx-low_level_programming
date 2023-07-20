a3ec2b9130c2b6095b7510dc2654d7a0cafc5035
root <lkulecho2001@gmail.com>
2023-07-20 06:54:54 -0700

    Print \ an n number of times

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_diagonal - print \
+ * @n: the parameter
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int a;
+
+	for (a = 0; a < n; a++)
+		_putchar('\');
+	_putchar('\n');
+}
