e8f10f540223e88f9afe263e126b2fcf3eae0e12
root <lkulecho2001@gmail.com>
2023-08-01 15:38:12 -0700

    search a string for any of a set of bytes.

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strpbrk - function to return the first occurence of the a string
+ * @s: source string
+ * @accept: target string
+ * Return: s
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	ant a = 0, b = 0;
+
+	while (s[a] != '\0')
+	{
+		while (accept[b] != '\0')
+		{
+			if (s[a] == accept[b])
+				return (s + a);
+			b++;
+		}
+		a++;
+	}
+	return (0);
+}
