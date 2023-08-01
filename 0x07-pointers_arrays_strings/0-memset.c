4b861864857d0f584f9e1c7a57360235508d793b
root <lkulecho2001@gmail.com>
2023-08-01 15:12:19 -0700

    Fill memory with a constant byte

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memset - fill the memory with a constant byte
+ * @s: target memory
+ * @b: source bytes
+ * @n: byte limiter
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int a = 0;
+
+	while (a < n;)
+		s[a] = b;
+		a++;
+	return (s);
+}
