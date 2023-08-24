9af865c4c3f0ec018bb3d4b16dd018c38c600113
root <lkulecho2001@gmail.com>
2023-08-24 16:16:36 -0700

    Function that frees a list

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,16 @@
+section .data
+	hello db "Hello, Holberton", 10
+
+section .text
+	global main
+
+main:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, hello
+	mov rdx, 17
+	syscall
+
+	mov rax, 60
+	xor rdi, rdi
+	syscall
